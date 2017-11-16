46f8661c27f0d4b21340240c73d3cfb1e0d9f180
ThierryO <Thierry.Onkelinx@inbo.be>
2017-11-16 16:02:13 +0100

    add profiling to analysis script

--- analysis.R
+++ analysis.R
@@ -2,4 +2,12 @@
 
 library(n2kanalysis)
 cat(argv, sep = "\n")
-fit_model(bucket = argv[1], x = argv[3], project = argv[2])
+p <- profvis::profvis({
+  fit_model(bucket = argv[1], x = argv[3], project = argv[2])
+})
+aws.s3::s3write_using(
+  x = p,
+  FUN = htmlwidgets::saveWidget,
+  object = sprintf("%s/profile/%s.html", argv[2], argv[3]),
+  bucket = argv[1]
+)
