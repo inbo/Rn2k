11529c1a0230c1e0adf211e0cf1a7002f929c417
Thierry Onkelinx <thierry.onkelinx@inbo.be>
2019-12-08 15:07:34 +0100

    don't profile the code

--- analysis.R
+++ analysis.R
@@ -1,12 +1,4 @@
 #!/usr/bin/env r
 
 library(n2kanalysis)
-cat(argv, sep = "\n")
-p <- profvis::profvis({
-  fit_model(bucket = argv[1], x = argv[3], project = argv[2], bash = TRUE)
-})
-aws.s3::s3saveRDS(
-  p,
-  object = sprintf("%s/profile/%s", argv[2], gsub("manifest", "rds", argv[3])),
-  bucket = argv[1]
-)
+fit_model(bucket = argv[1], x = argv[3], project = argv[2], bash = TRUE)
