649095ed25ac143f2e25efc24d55d35c15ece46b
Thierry Onkelinx <thierry.onkelinx@inbo.be>
2023-08-07 10:43:44 +0200

    use local argument of fit_model()

--- fit_model_aws.R
+++ fit_model_aws.R
@@ -1,5 +1,6 @@
 #!/usr/bin/env r
 
 library(n2kanalysis)
-cat(argv, sep = "\n")
-fit_model(bucket = argv[1], x = argv[3], project = argv[2])
+fit_model(
+  bucket = argv[1], x = argv[3], project = argv[2], local = "/n2knalysis"
+)
