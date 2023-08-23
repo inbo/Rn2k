96459a5ab1464de3d54b8e5e1dce0727481b31cd
Thierry Onkelinx <thierry.onkelinx@inbo.be>
2023-08-23 12:03:02 +0200

    also fit models with status equal to error

--- fit_model_aws.R
+++ fit_model_aws.R
@@ -2,5 +2,6 @@
 
 library(n2kanalysis)
 fit_model(
-  bucket = argv[1], x = argv[3], project = argv[2], local = "/n2kanalysis"
+  bucket = argv[1], x = argv[3], project = argv[2], local = "/n2kanalysis",
+  status = c("new", "waiting", "error")
 )
