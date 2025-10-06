eda7cba5c79b15ec1352868e4d6293f0bcf6c67b
Thierry Onkelinx <thierry.onkelinx@inbo.be>
2025-10-06 11:59:08 +0200

    ⬆️ Upgrade to R 4.5.1

--- update_docker.R
+++ update_docker.R
@@ -1,2 +1,5 @@
 library(pak)
-lockfile_create(pkg = c("n2kanalysis", "INLA", "git2rdata", "sn"))
+lockfile_create(
+  pkg = c("s2@1.1.7", "inbo/n2kanalysis", "aws.ec2metadata", "sn"),
+  upgrade = TRUE
+)
